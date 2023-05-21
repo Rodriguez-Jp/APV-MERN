3274525f43e29dd700d3cf334d962971b282fcef
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-05-21 01:25:24 -0500

    Terminando el backend, empezando el frontend

--- backend/models/Paciente.js
+++ backend/models/Paciente.js
@@ -1,27 +1,36 @@
 import moongose from "mongoose";
 
-const pacienteSchema = moongose.Schema({
-  nombre: {
-    type: String,
-    required: true,
+const pacienteSchema = moongose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    propietario: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    fechaAlta: {
+      type: Date,
+      required: true,
+      default: Date.now(),
+    },
+    sintomas: {
+      type: String,
+      required: true,
+    },
+    veterinario: {
+      type: moongose.Schema.Types.ObjectId,
+      ref: "Veterinario",
+    },
   },
-  propietario: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  fechaAlta: {
-    type: Date,
-    required: true,
-  },
-  veterinario: {
-    type: moongose.Schema.Types.ObjectId,
-    ref: "Veterinario",
-  },
-  timestamps: true,
-});
+  { timestamps: true }
+);
 
 const Paciente = moongose.model("Paciente", pacienteSchema);
+
+export default Paciente;
