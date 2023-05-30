bc28a17e443356ebdfc8230ad75d296b595ffbad
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-05-29 23:16:40 -0500

    agregando codigo CORS

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,26 @@ import conectarDB from "./config/db.js";
 import dotenv from "dotenv";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 conectarDB();
+
+//Validacion CORS
+const dominiosPermitidos = ["http://localhost:5173"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      //El origin esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api/veterinarios", veterinarioRoutes);
