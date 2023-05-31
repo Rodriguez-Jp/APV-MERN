aac191079fcf158979d1182af931bc92070c33c7
Juan Pablo Rodriguez Becerra <juanrdrz16@gmail.com>
2023-05-30 20:23:11 -0500

    Agregando variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 conectarDB();
 
 //Validacion CORS
-const dominiosPermitidos = ["http://localhost:5173"];
+const dominiosPermitidos = [process.env.FRONTEND_URL];
 const corsOptions = {
   origin: function (origin, callback) {
     if (dominiosPermitidos.indexOf(origin) !== -1) {
